4aa1e0a613aca8375d2d9bba734cbb4a8a050490
MIDHUN-REDDY <142382927+MIDHUN-REDDY@users.noreply.github.com>
2024-08-16 19:10:25 +0530

    Update BUG TRACKING SYSTEM.py

--- BUG TRACKING SYSTEM.py
+++ BUG TRACKING SYSTEM.py
@@ -1,5 +1,4 @@
-Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+
 import mysql.connector
 
 def get_db_connection():
